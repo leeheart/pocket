539a66c2f56eb989e3150a705efd6d4c25589afc
leehe <lihechn@outlook.com>
2020-03-27 19:54:41 +0800

    创建三个页面

---
+++ src/views/Statistics.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    statistics页面
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Statistics'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
