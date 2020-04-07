1ac149a1e5426487b6a8b619f6b6cb934cb16a34
leehe <lihechn@outlook.com>
2020-04-07 17:34:10 +0800

    封装 recordList 的增删改查

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,16 +15,15 @@
   import FormItem from '@/components/Money/FormItem.vue';
   import Types from '@/components/Money/Types.vue';
   import NumberPad from '@/components/Money/NumberPad.vue';
-  import {Component, Watch} from 'vue-property-decorator';
-  import recordListModel from '@/models/recordListModel';
-  import tagListModel from '@/models/tagListModel';
+  import {Component} from 'vue-property-decorator';
+
 
   @Component({
     components: {NumberPad, Types, FormItem, Tags}
   })
   export default class Money extends Vue {
-    tags = tagListModel.fetch();
-    recordList = recordListModel.fetch();
+    tags = window.tagList;
+    recordList = window.recordList;
     record: RecordItem = {
       tags: [],
       notes: '',
@@ -45,12 +44,7 @@
     }
 
     saveRecord() {
-      recordListModel.create(this.record);
-    }
-
-    @Watch('recordList')
-    onRecordChange() {
-      recordListModel.save(this.recordList);
+      window.createRecord(this.record);
     }
   }
 </script>
